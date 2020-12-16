a174fc01d15bd3933cdad0ed249ad7247098fbf0
cooper25 <me@loertis.dev>
2020-12-16 16:36:48 -0600

    fucking hell

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+require("../components/main.css")
+
+const IndexPage = () => {
+  return (
+    <body>
+      <div className={"thmbnl_cont"}>
+        <div className={"thmbnl-image_cont"}>
+          <div id={"thmbnl"}>
+            <div id={"txt"}>Placeholder Text</div>
+          </div>
+          <div id={"thmbnl"}>
+            <div id={"txt"}>Placeholder Text</div>
+          </div>
+          <div id={"thmbnl"}>
+            <div id={"txt"}>Placeholder Text</div>
+          </div>
+          <div id={"thmbnl"}>
+            <div id={"txt"}>Placeholder Text</div>
+          </div>
+          <div id={"thmbnl"}>
+            <div id={"txt"}>Placeholder Text</div>
+          </div>
+          <div id={"thmbnl"}>
+            <div id={"txt"}>Placeholder Text</div>
+          </div>
+        </div>
+      </div>
+    </body>
+  )
+}
+
+export default IndexPage
